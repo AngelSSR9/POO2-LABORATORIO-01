36ca6a51321d1084c0f0d515fe977db626f4c3ab
Angel <angel.serrano@usil.pe>
2023-08-18 16:46:22 -0500

    Creando la clase CirculoTest

---
+++ src/main/java/CirculoTest.java
@@ -0,0 +1,11 @@
+public class CirculoTest {
+    public static void main(String[] args) {
+        Circulo c1 = new Circulo();
+        System.out.println("Radio del circulo por defecto: " + c1.getRadio());
+        System.out.println("Area del circulo por defecto: " + c1.getArea());
+
+        Circulo c2 = new Circulo(6);
+        System.out.println("Radio del circulo modificado: " + c2.getRadio());
+        System.out.println("Area del circulo modificado: " + c2.getArea());
+    }
+}
